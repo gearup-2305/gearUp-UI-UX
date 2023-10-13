22930b1901cd9401fd4cbce92b7c7e3069ab12d0
Scotty Brown <scottybrown98@gmail.com>
2023-10-13 17:38:26 -0500

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@
 import './App.css';
 import Header from './components/Header/Header'
 import { useEffect, useState } from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
-import { LOAD_ARTISTS } from './graphQL/Queries';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import { LOAD_ARTISTS } from './graphQL/Queries';
+import { LOAD_ARTISTS } from './graphQL/Queries'
 
 const client = new ApolloClient({
   uri: 'https://c4fa458b-51b7-48f5-9cc1-ec7ad47e1e6d.mock.pstmn.io/graphql',

f508ec1e3b3c79614c5a3da3854e7edd34d077ff
Scotty Brown <scottybrown98@gmail.com>
2023-10-13 17:10:19 -0500

    Setup Apollo client

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 // import logo from './logo.svg';
 // import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { ApolloClient, ApolloProvider, HttpLink, from, useQuery} from '@apollo/client'
-import { onError } from '@apollo/client/link/error'
 import Header from './components/Header/Header'
-import { LOAD_ARTISTS } from './GraphQL/Queries';
 import { useEffect, useState } from 'react';
-// import {} from ''
-
-const errorLink = onError(({graphqlErrors, networkError}) => {
-  if (graphqlErrors) {
-      graphqlErrors.map(({message, location, path}) => {
-      console.log(`Graphql Error ${message} ${location} ${path}`)
-    })
-  }
-})
-
-const link = from([
-  errorLink,
-  new HttpLink({uri: "https://c4fa458b-51b7-48f5-9cc1-ec7ad47e1e6d.mock.pstmn.io/graphql"})
-])
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
+import { LOAD_ARTISTS } from './graphQL/Queries';
 
 const client = new ApolloClient({
-  link: link, 
-  // cache: new InMemoryCache(),
-})
+  uri: 'https://c4fa458b-51b7-48f5-9cc1-ec7ad47e1e6d.mock.pstmn.io/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  // const [artists, setArtists] = useState({})
-  const { error, loading, data } = useQuery(LOAD_ARTISTS)
+  const [artists, setArtists] = useState({})
 
-  useEffect(() => {
-    console.log(data)
-    // if(data) {
-    //   setArtists(data.artists)
-    // }
-  }, [data])
+    client
+    .query({
+      query: LOAD_ARTISTS
+    })
+    .then((result) => setArtists(result.data.artists));
+
 
   return (
     <ApolloProvider client={client}>

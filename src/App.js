72541cb8f6875138ebd72e05f791a61e30e78dd5
ScottyB <130028791+Scotty-Brown@users.noreply.github.com>
2023-10-13 17:44:24 -0500

    Cleanup linters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { onError } from '@apollo/client/link/error'
+// import { onError } from '@apollo/client/link/error'
 import Header from './components/Header/Header'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { LOAD_ARTISTS } from './GraphQL/Queries';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 const client = new ApolloClient({
   uri: 'https://c4fa458b-51b7-48f5-9cc1-ec7ad47e1e6d.mock.pstmn.io/graphql',

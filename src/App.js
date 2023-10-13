b2866c59fc358de77c766b5bc48a847021414e00
Alex Peterson <aspeterson.126@gmail.com>
2023-10-13 12:52:37 -0500

    Add initial header w/ logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
     </div>
   );
 }
 
 export default App;
+
+// componenets
+  // Homepage
+    // Login component
+  // Nav
+  // Community Board - acts as container
+    // Filter Donation Requests
+    // Donation Request Cards
+  // Donation Request Form
+    // Login component - conditionally render depending on if user is logged in
+  // Profile
+
+
+
+  // backend questions:
+  // donation active: true / false --> backend switches to false when donation request fulfilled or amount met
+  // edge case: if donation has 20 dollars needed, what if someone donates 30? do we control the form? do we allow them 
+  // to donate more? 

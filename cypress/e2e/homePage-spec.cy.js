8ea855460c94d84d004603321f0793c1dfc3f5d0
Alex Peterson <aspeterson.126@gmail.com>
2023-10-14 15:08:26 -0500

    Add home page testing

---
+++ cypress/e2e/homePage-spec.cy.js
@@ -0,0 +1,48 @@
+describe('Test Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  });
+
+    it('should display header', () => {
+      cy.get('.header')
+         cy.get('.title-wrapper')
+      cy.get('h1')
+        .should('contain', 'GearUp')
+    });
+  
+    it('should display nav bar', () => {
+      cy.get('.nav-wrapper')
+      cy.get('[href="/donation-request"]')
+      cy.get('[href="/"]')
+    });
+  
+    it('Visit donation request age from home page', () => {
+      cy.visit('http://localhost:3000/')
+      cy.get('.nav-wrapper')
+      cy.get('[href="/donation-request"]').click()
+      cy.visit('http://localhost:3000/donation-request')
+    });
+  
+    it('Visit login page from home page', () => {
+      cy.url().should('eq', 'http://localhost:3000/')
+      cy.get('.home-container')
+      cy.get('.description-container')
+      cy.get('.login-container')
+      cy.get('[href="/login-form"]').click()
+      cy.visit('http://localhost:3000/login-form')
+  
+  });
+  
+    it('Should contain hero image with GearUp description', () => {
+      cy.url().should('eq', 'http://localhost:3000/')
+      cy.get('.home-container')
+      cy.get('.description-container')
+      cy.get('h2')
+        .should('contain', 'Help Artists Create Art')
+      cy.get('p')
+        .should('contain', 'GearUp connects a widespread community of donors with thriving artists who are eager to bring their artistic vision to life.')
+  
+    })
+  
+
+})

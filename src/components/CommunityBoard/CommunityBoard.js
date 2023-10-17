7e4cf6fd6224500b1681d61df3780ce915c92d98
Scotty Brown <scottybrown98@gmail.com>
2023-10-17 13:11:47 -0500

    Add donation request cards to CommBoard component

--- src/components/CommunityBoard/CommunityBoard.js
+++ src/components/CommunityBoard/CommunityBoard.js
@@ -1,27 +1,32 @@
 import './CommunityBoard.css'
-// import { useQuery } from '@apollo/client';
-// import { useState, useEffect } from 'react';
-// import { LOAD_ARTISTS } from '../../GraphQL/Queries';
-// import Projects from '../Projects/Projects'
+import { useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
+import { LOAD_ARTISTS } from '../../GraphQL/Queries';
+import Projects from '../Projects/Projects'
 
 const CommunityBoard = () => {
-    // const { loading, error, data } = useQuery(LOAD_ARTISTS);
-    // const [donations, setDonations] = useState({})
+    const { loading, error, data } = useQuery(LOAD_ARTISTS);
+    const [donations, setDonations] = useState({})
     
-    // useEffect(() => {
-    //     if (error) {
-    //       console.error('Error fetching data:', error);
-    //       return <p>Error: {error.message}</p>;
-    //     }
-    //     if (!loading && data) {
-    //       const artists = data.artists;
-    //       setDonations(artists);
-    //     }
-    //   }, [loading, error, data]);
+    useEffect(() => {
+        if (error) {
+          console.error('Error fetching data:', error);
+          return <p>Error: {error.message}</p>;
+        }
+        if (!loading && data) {
+          const artists = data;
+          setDonations(artists);
+        }
+      }, [loading, error, data]);
 
-    //   return (
-    //     <Projects donations={donations} />
-    //   )
+      const allDonationRequests = data && donations?.artists?.map( user => {
+        // console.log(user)
+        return (<Projects user={user} />)
+      })
+
+      return (
+        <div className='all-donation-requests-container'>{allDonationRequests}</div>
+      )
 }
 
 export default CommunityBoard

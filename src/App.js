b12b108c4a28ae71d17247cf60d00739e7a1b99b
Alex Peterson <aspeterson.126@gmail.com>
2023-10-16 20:17:41 -0500

    Remove commented out code

--- src/App.js
+++ src/App.js
@@ -6,26 +6,24 @@ import Login from './components/Login/Login';
 import CommunityBoard from './components/CommunityBoard/CommunityBoard';
 import Profile from './components/Profile/Profile';
 
-import { Routes, Route,  } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
-
-
 
 function App() {
   const [loginAccess, setLoginAccess] = useState(false)
+  const [logOutAccess, setLogOutAccess] = useState(false)
   const [loggedInUser, setLoggedInUser] = useState({})
 
   
   return (
     <>
         <Header/>
-        {loginAccess && <Login setLoginAccess={setLoginAccess} loginAccess={loginAccess} setLoggedInUser={setLoggedInUser}/>}
         <Routes>
-          <Route path="/" element={<HomePage setLoginAccess={setLoginAccess} />}/>
+          <Route path="/" element={<HomePage setLoginAccess={setLoginAccess} logOutAccess={logOutAccess} setLogOutAccess={setLogOutAccess}/>}/>
           <Route path="donation-request" element={<DonationRequest/>}/>
           <Route path='community-board' element={<CommunityBoard/>}/>
           <Route path='profile' element={<Profile loginAccess={loginAccess} loggedInUser={loggedInUser}/>}/>
-          {/* <Route path="login-form" element={<Login/>}/> */}
+          <Route path="login-form" element={<Login setLoginAccess={setLoginAccess} loginAccess={loginAccess} setLoggedInUser={setLoggedInUser} setLogOutAccess={setLogOutAccess}/>}/>
       </Routes>
    </>  
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,10 @@
 import './Login.css'
 import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom'
 import { LOAD_ARTISTS } from '../../GraphQL/Queries';
 
-function LoginForm({loginAccess, setLoginAccess, setLoggedInUser}) {
+function Login({ setLoggedInUser, loginAccess, setLogOutAccess}) {
   const [password, setPassword] = useState('$2a$12$Qm68zSOdKXCoQ3NeDJBYdOQQ4toyKZbCTHhUs9IOSixebWP89MadC')
   const [allArtists, setAllArtists] = useState([])
   const [singleArtist, setSingleArtist] = useState(null)
@@ -32,35 +33,39 @@ function LoginForm({loginAccess, setLoginAccess, setLoggedInUser}) {
   }, [allArtists, password]);
 
   return (
-    <div className={`login-form-container ${loginAccess ? 'active' : ''}`}>
-      <form className="login-form">
-        <h2>Please Log In:</h2>
-        <label className='login-form-label' htmlFor='Username'>Username:</label>
-        <input
-          className="form-input"
-          type="text"
-          placeholder="Username"
-          name="Username"
-          value={'userOne1!'}
-          readOnly
-        />
-        <label className='login-form-label' htmlFor='Password'>Password:</label>
-        <input
-          className="form-input"
-          type="text"
-          placeholder="Password"
-          name="Password"
-          defaultValue={password}
-        />
-        <button className='login-button' onClick={() => {
-          setLoginAccess(false)
-          setLoggedInUser(singleArtist)
-          console.log(singleArtist) }}>Login</button>
-        <button className='login-button'>Cancel</button>
-      </form>
+    <div className={`login-form-container`}>
+      {loginAccess ? (
+        <form className="login-form">
+          <h2>Please Log In:</h2>
+          <label className='login-form-label' htmlFor='Username'>Username:</label>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Username"
+            name="Username"
+            value={'userOne1!'}
+            readOnly
+          />
+          <label className='login-form-label' htmlFor='Password'>Password:</label>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Password"
+            name="Password"
+            defaultValue={password}
+          />
+          <Link className='login-link' to={`/profile`} onClick={() => {
+            setLoggedInUser(singleArtist) 
+            setLogOutAccess(true)
+            }}>Login.</Link>
+        </form>
+      ) : (
+        <div>
+          You are already logged in.
+        </div>
+      )}
     </div>
   )
 }
 
-export default LoginForm;
-
+export default Login;

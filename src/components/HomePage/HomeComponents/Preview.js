7fb526b2f1ae5b0a9ee41af20e2a0f3c611e3405
Scotty Brown <scottybrown98@gmail.com>
2023-10-20 15:23:34 -0500

    Adjust styling with added Link in Preview comp

--- src/components/HomePage/HomeComponents/Preview.js
+++ src/components/HomePage/HomeComponents/Preview.js
@@ -14,8 +14,9 @@ const Preview = () => {
         <>
         <h2 className='description-heading preview-heading'>Checkout some recent donation requests:</h2>
         <div className='preview-container'>
+        <div className='preview-container-cards'>
         {mostRecentPosts.map(post => (
-          <div key={post.id} className='donation-card'>
+          <div key={post.id} className='donation-card center'>
             <h2>Title: {post.title}</h2>
             <p>Details: {post.details}</p>
             <p>Requested Amount: ${post.requestedAmount}</p>
@@ -23,6 +24,8 @@ const Preview = () => {
             <Link className='donation-offer' to={`/${post.id}`} state={{post}}>Donate</Link>
           </div>
         ))}
+        </div>
+        <Link className='explore-more-button' to={`/community-board`}>Explore more opportunities to support!</Link>
         </div>
         </>
     )

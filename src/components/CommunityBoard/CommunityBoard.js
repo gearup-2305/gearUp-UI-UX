5c29f79632f720a014eda1634eb44b3c0d94d992
Scotty Brown <scottybrown98@gmail.com>
2023-10-21 18:48:00 -0500

    Add form setup for filter

--- src/components/CommunityBoard/CommunityBoard.js
+++ src/components/CommunityBoard/CommunityBoard.js
@@ -5,11 +5,14 @@ import { LOAD_ARTISTS } from '../../GraphQL/Queries';
 import DonationCard from '../DonationCard/DonationCard'
 import Loading from '../Loading/Loading';
 import Error from '../Error/Error';
+import Filter from '../Filter/Filter';
 
 
 const CommunityBoard = () => {
     const { loading, error, data, refetch } = useQuery(LOAD_ARTISTS);
     const [donations, setDonations] = useState({})
+    const [noDonationsSearch, setNoDonationsSearch] = useState(false)
+    const [firstProjectSearch, setFirstProjectSearch] = useState(false)
 
     useEffect(() => {
       refetch()
@@ -25,18 +28,52 @@ const CommunityBoard = () => {
       return <Error error={error}/>
     }
 
-    const artistsWithFilteredPosts = data&& donations?.artists?.map(artists => ({
-      ...artists,
-      posts: artists.posts.filter(post => post.requestedAmount > post.currentAmount)
-    }))
+    const artistsWithFilteredPosts = data && donations?.artists?.map(artists => {
+
+      // artists.filter
+
+      let filteredPosts = artists.posts;
+      console.log('filteredposts' )
+    
+      if (noDonationsSearch) {
+        const lastPost = filteredPosts[filteredPosts.length - 1]
+        console.log('lastpost', lastPost)
+        if (lastPost?.currentAmount === 0) (
+          filteredPosts = lastPost
+          )
+          console.log('nodations', filteredPosts)
+      }
+    
+      if (firstProjectSearch) {
+        if (filteredPosts.length === 1) {
+          return filteredPosts
+        }
+        console.log('firstproject', filteredPosts)
+      }
+    
+      // Return the artist with the filtered posts
+      return {
+        ...artists,
+        posts: filteredPosts,
+      };
+    });
+
+    console.log(artistsWithFilteredPosts)
+
+    // const artistsWithFilteredPosts = data && donations?.artists?.map(artists => ({
+    //   ...artists,
+    //   posts: artists.posts.filter(post => post.requestedAmount > post.currentAmount)
+    // }))
 
       const allDonationRequests = artistsWithFilteredPosts?.map( user => {
         return (<DonationCard key={user.id} user={user} />)
       })
 
       return (
+        <div className='community-board-container'>
+        <Filter setNoDonationsSearch={setNoDonationsSearch} setFirstProjectSearch={setFirstProjectSearch} noDonationsSearch={noDonationsSearch} firstProjectSearch={firstProjectSearch}/>
         <div className='all-donation-requests-container'>{allDonationRequests}</div>
-        
+        </div>
       )
 }
 
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,130 @@
+import './Filter.css'
+
+const Filter = ({setNoDonationsSearch, setFirstProjectSearch, noDonationsSearch, firstProjectSearch}) => {
+    function handleSearchInput(e) {
+        if (e.target.id === 'no-donations') {
+            setToFalse()
+            // setFirstProjectSearch(false)
+            setNoDonationsSearch(true)
+        } else if (e.target.id === 'first-projects') {
+            setToFalse()
+            // setNoDonationsSearch(false)
+            setFirstProjectSearch(true)
+        }
+    }
+
+    function setToFalse() {
+        setFirstProjectSearch(false)
+        setNoDonationsSearch(false)
+    }
+
+    return (
+        <div className='form-section'>
+        <form>
+        <h2 className='form-heading'>Maximize Your Impact</h2>
+        <div className='no-donations-section'>
+            <div className='input-label-wrapper'>
+            <div className='input-wrapper'>
+                <input
+                type='checkbox'
+                id='no-donations'
+                name='noDonations'
+                checked={noDonationsSearch}
+                onChange={event => handleSearchInput(event)} 
+                />
+            </div>
+            <div className='label-wrapper'>
+                <label htmlFor='no-donations'>Artist with No Donations</label>
+            </div>
+            </div>
+        </div>
+        <div className='first-donations-section'>
+            <div className='input-label-wrapper'>
+            <div className='input-wrapper'>
+                <input
+                type='checkbox'
+                id='first-projects'
+                name='firstProjects'
+                checked={firstProjectSearch} 
+                onChange={event => handleSearchInput(event)} 
+                />
+            </div>
+            <div className='label-wrapper'>
+                <label htmlFor='first-projects'>Artist's First Project</label>
+            </div>
+            </div>
+        </div>
+        {/* <div className='state-selector'>
+            <label htmlFor='state'>Select State:</label>
+            <select
+            id='state'
+            name='state'
+            // value={selectedState}
+            // onChange={handleStateChange}
+            >
+            <option value=''>Select a state</option>
+            <option value='AL'>AL</option>
+            <option value='AK'>AK</option>
+            <option value='AZ'>AZ</option>
+            <option value='AR'>AR</option>
+            <option value='CA'>CA</option>
+            <option value='CO'>CO</option>
+            <option value='CT'>CT</option>
+            <option value='DE'>DE</option>
+            <option value='FL'>FL</option>
+            <option value='GA'>GA</option>
+            <option value='HI'>HI</option>
+            <option value='ID'>ID</option>
+            <option value='IL'>IL</option>
+            <option value='IN'>IN</option>
+            <option value='IA'>IA</option>
+            <option value='KS'>KS</option>
+            <option value='KY'>KY</option>
+            <option value='LA'>LA</option>
+            <option value='ME'>ME</option>
+            <option value='MD'>MD</option>
+            <option value='MA'>MA</option>
+            <option value='MI'>MI</option>
+            <option value='MN'>MN</option>
+            <option value='MS'>MS</option>
+            <option value='MO'>MO</option>
+            <option value='MT'>MT</option>
+            <option value='NE'>NE</option>
+            <option value='NV'>NV</option>
+            <option value='NH'>NH</option>
+            <option value='NJ'>NJ</option>
+            <option value='NM'>NM</option>
+            <option value='NY'>NY</option>
+            <option value='NC'>NC</option>
+            <option value='ND'>ND</option>
+            <option value='OH'>OH</option>
+            <option value='OK'>OK</option>
+            <option value='OR'>OR</option>
+            <option value='PA'>PA</option>
+            <option value='RI'>RI</option>
+            <option value='SC'>SC</option>
+            <option value='SD'>SD</option>
+            <option value='TN'>TN</option>
+            <option value='TX'>TX</option>
+            <option value='UT'>UT</option>
+            <option value='VT'>VT</option>
+            <option value='VA'>VA</option>
+            <option value='WA'>WA</option>
+            <option value='WV'>WV</option>
+            <option value='WI'>WI</option>
+            <option value='WY'>WY</option>
+            </select> */}
+        {/* </div> */}
+        {/* <button className='button'>
+            Submit
+        </button> */}
+        </form>
+        {/* <button className='button'>
+        Clear Filters
+        </button> */}
+        </div>
+
+    )
+}
+
+export default Filter

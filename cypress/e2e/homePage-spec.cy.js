b266383afc6527b8d2dfbd68fe8e36559d95e8a7
Alex Peterson <aspeterson.126@gmail.com>
2023-10-22 14:18:19 -0500

    Edit homepage testing to pass

--- cypress/e2e/homePage-spec.cy.js
+++ cypress/e2e/homePage-spec.cy.js
@@ -14,22 +14,22 @@ describe('Test Home Page', () => {
   
     it('should display nav bar', () => {
       cy.get('.nav-wrapper')
-      cy.get('[href="/donation-request"]')
       cy.get('[href="/"]')
-    });
-  
-    it('Visit donation request age from home page', () => {
-      cy.get('.nav-wrapper')
-      cy.get('[href="/donation-request"]').click()
-      cy.visit('http://localhost:3000/donation-request')
+      cy.get('[href="/login-form"]')
+      cy.get('[href="/community-board"]')
     });
   
     it('Visit login page from home page', () => {
       cy.url().should('eq', 'http://localhost:3000/')
       cy.get('.login-link').click()
       cy.visit('http://localhost:3000/login-form')
-  
   });
+
+  it('Visit login page from home page', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('.login-link').click()
+    cy.visit('http://localhost:3000/login-form')
+});
   
     it('Should contain hero image with GearUp description', () => {
       cy.url().should('eq', 'http://localhost:3000/')

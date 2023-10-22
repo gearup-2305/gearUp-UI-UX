40a504f42d0d8bb6b107dd3e67d2a2e45139aa48
Alex Peterson <aspeterson.126@gmail.com>
2023-10-22 14:05:17 -0500

    Add in toggle request donation from nav depending on logged in/logged out user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header = ({ profileAccess }) => {
   const profileLoginToggle = profileAccess ? (
-    <NavLink to={'/profile'} className='nav-link'>🎨 Profile</NavLink>
+    <div>
+      <NavLink to={`/donation-request`} className='nav-link'>Request a Donation</NavLink>
+      <NavLink to={'/profile'} className='nav-link'>🎨 Profile</NavLink>
+    </div>
   ) : (
     <NavLink to={'/login-form'} className='nav-link'>Login</NavLink>
   );
@@ -18,9 +21,8 @@ const Header = ({ profileAccess }) => {
       </div>
       <div className='nav-wrapper'>
         <NavLink to={`/`} className='nav-link'>Home</NavLink>
-        <NavLink to={`/donation-request`} className='nav-link'>Request a Donation</NavLink>
         <NavLink to={'/community-board'} className='nav-link'>Community Board</NavLink>
-        {profileLoginToggle}
+          {profileLoginToggle}
       </div>
     </header>
   );

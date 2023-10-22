5da4d21f8766debe0c9a741b99a87932204598f7
Scotty Brown <scottybrown98@gmail.com>
2023-10-22 16:20:17 -0500

    Add all testing for community board

--- cypress/e2e/community-board-spec.cy.js
+++ cypress/e2e/community-board-spec.cy.js
@@ -7,19 +7,78 @@ describe('Community Board', () => {
   
   it('Should show all donation requests that arent fully funded', () => {
     cy.get('.donation-card').first().then((card) => {
-      cy.wrap(card).find('.profile-image-card').should('have.attr', 'src', 'https://example.com/artist2.jpg');
-      cy.wrap(card).find('.personal-details-container h3').should('contain', 'Artist 2');
-      cy.wrap(card).find('.personal-details-container p').should('contain', 'City 2');
-      cy.wrap(card).find('.project-details-card h3').should('contain', 'Sculpture 3');
-      cy.wrap(card).find('.single-project-card p').should('contain', 'Details: Another amazing sculpture without donations');
-      cy.wrap(card).find('.donation-amounts :nth-child(1)').should('contain', 'Requested Amount: $900');
-      cy.wrap(card).find('.donation-amounts :nth-child(2)').should('contain', 'Amount Raised: $0');
-      cy.wrap(card).find('.donate-button-container .donation-offer').should('exist');
+      cy.checkDonationRequestCard(card, 'https://example.com/artist2.jpg', 'Artist 2', 'City 2', 'Sculpture 3', 'Details: Another amazing sculpture without donations', 'Requested Amount: $900', 'Amount Raised: $0')
     })
+
+    cy.get('.donation-card').last().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 1', 'Details: A beautiful painting', 'Requested Amount: $1000', 'Amount Raised: $750')
+    })
+  })
+
+  it('Should filter community board by requests with No Donations', () => {
+    cy.get('#no-donations').click({ force: true })
+    cy.get('.donation-card').first().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist2.jpg', 'Artist 2', 'City 2', 'Sculpture 3', 'Details: Another amazing sculpture without donations', 'Requested Amount: $900', 'Amount Raised: $0')
+    })
+})
+
+  it('Should filter community board by first donation request from artists that havent met their requested amount goal', () => {
+    cy.get('#first-projects').click({ force: true })
+    cy.get('.donation-card').first().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 1', 'Details: A beautiful painting', 'Requested Amount: $1000', 'Amount Raised: $750')
+    })
+  
+  })
+
+  it('Should filter community board by state', () => {
+    cy.get('#state').select('CO')
+    cy.get('.donation-card').first().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 2', 'Details: Another beautiful painting', 'Requested Amount: $1200', 'Amount Raised: $1200')
+    })
+
+    cy.get('.donation-card').last().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 1', 'Details: A beautiful painting', 'Requested Amount: $1000', 'Amount Raised: $750')
+    })
+    
+    })
+
+  it('Should be able to clear form filters', () => {
+    cy.get('#state').select('CO')
+    cy.get('.donation-card').first().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 2', 'Details: Another beautiful painting', 'Requested Amount: $1200', 'Amount Raised: $1200')
+    })
+
+    cy.get('button').click()
+
+    cy.get('.donation-card').last().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 1', 'Details: A beautiful painting', 'Requested Amount: $1000', 'Amount Raised: $750')
+    })
+
+    cy.get('#no-donations').click({ force: true })
 
     cy.get('.donation-card').first().then((card) => {
       cy.checkDonationRequestCard(card, 'https://example.com/artist2.jpg', 'Artist 2', 'City 2', 'Sculpture 3', 'Details: Another amazing sculpture without donations', 'Requested Amount: $900', 'Amount Raised: $0')
     })
 
-  })
+    cy.get('button').click()
+
+    cy.get('.donation-card').last().then((card) => {
+      cy.checkDonationRequestCard(card, 'https://example.com/artist1.jpg', 'Artist 1', 'Denver', 'Painting 1', 'Details: A beautiful painting', 'Requested Amount: $1000', 'Amount Raised: $750')
+    })
+    })
+
+    // it('Should be able to clear form filters', () => {
+    //   cy.get('.donation-card').first().then((card) => {
+    //     cy.wrap(card).find('.donation-offer').click()
+    //   })
+    //   cy.url('should', 'contain', 'http://localhost:3000/203')
+  
+    // })
+
+    it('Should display message letting user know if filter has no results', () => {
+      cy.get('#state').select('TX')
+      cy.get('.no-results').should('exist')
+      cy.get('.no-results').should('contain', 'Sorry, no results found matching your filter criteria.')
+  
+    })
 })

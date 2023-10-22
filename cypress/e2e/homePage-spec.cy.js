3e99e4cad767d1099f4947015bc390ee9ebe6042
Alex Peterson <aspeterson.126@gmail.com>
2023-10-21 21:14:30 -0500

    Add testing for all links/routing on home page

--- cypress/e2e/homePage-spec.cy.js
+++ cypress/e2e/homePage-spec.cy.js
@@ -15,13 +15,25 @@ describe('Test Home Page', () => {
     cy.get('[href="/"]')
   })
 
-  it('Visit donation request age from home page', () => {
+  it('Visit donation request age from navigation', () => {
     cy.get('.nav-wrapper')
     cy.get('[href="/donation-request"]').click()
     cy.visit('http://localhost:3000/donation-request')
   })
 
-  it('Visit login page from home page', () => {
+  it('Visit login page from navigation', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('.login-link').click()
+    cy.visit('http://localhost:3000/login-form')
+  })
+
+  it('Visit communinty board page from navigation', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('.login-link').click()
+    cy.visit('http://localhost:3000/login-form')
+  })
+
+  it('Visit request donation page from navigation', () => {
     cy.url().should('eq', 'http://localhost:3000/')
     cy.get('.login-link').click()
     cy.visit('http://localhost:3000/login-form')
@@ -34,22 +46,6 @@ describe('Test Home Page', () => {
     cy.get('p').should(
       'contain',
       'GearUp connects a widespread community of donors with thriving artists who are eager to bring their artistic vision to life.'
-    )
-  })
-
-  it('Should contain testimonial section with two images and descriptive alt text', () => {
-    cy.get('.testimonial-container')
-    cy.get('h2').should('contain', 'Real Life Impact')
-    cy.get('p').should(
-      'contain',
-      'GearUp has proven to be a lifeline for artists, offering a unique opportunity to connect with individuals who share a passion for creativity and a deep appreciation for the arts. The impact of support extends far beyond the financial aspect; it is a source of inspiration and affirmation that artists are valued.'
-    )
-    cy.get('.testimonial-artist-container')
-    cy.get('h2').should('contain', 'Creating Art and Community')
-    cy.get('#quote').should(
-      'have.attr',
-      'alt',
-      'Text bubble with quote stating: "This platform connected me with a community of generous donors who believed in my artistic vision and made it a reality. Their contribution not only provided me with the necessary resources but also gave me the confidence and motivation to push my boundaries as an artist." quoted from Rashid Johnson, Painter & Sculptor, Long Island, N.Y. '
     )
   })
 
@@ -85,5 +81,17 @@ describe('Test Home Page', () => {
     cy.get('p').should('contain', 'Details: Project Description Sample')
     cy.get('p').should('contain', 'Requested Amount: $100')
     cy.get('p').should('contain', 'Current Amount: $0')
+  })
+
+  it('Should take user to offer donation page click recent project donate button', () => {
+    cy.get('.preview-container-cards > :nth-child(3)')
+    cy.get(':nth-child(3) > .donation-offer').click()
+    cy.url().should('eq', 'http://localhost:3000/51')
+  })
+
+  it('Should take user to offer donation page click recent project donate button', () => {
+    cy.get('.preview-container')
+    cy.get('.explore-more-button').click()
+    cy.url().should('eq', 'http://localhost:3000/community-board')
   })
 })
